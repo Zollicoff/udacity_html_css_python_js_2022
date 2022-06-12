9b2e4b531e69f789b2530c815ef459269e746a5b
Zachary A. Hampton <95104559+Zollicoff@users.noreply.github.com>
2022-06-11 22:58:56 -0700

    commit

--- Python/Udacity/Intro_to_Python_1/silly_sentences/words.py
+++ Python/Udacity/Intro_to_Python_1/silly_sentences/words.py
@@ -1,22 +1,21 @@
-import random
-import words
+nouns = ['apple', 'ball', 'cat', 'dog', 'elephant',
+         'fish', 'goat', 'house', 'iceberg', 'jackal',
+         'king', 'llama', 'monkey', 'nurse', 'octopus',
+         'pie', 'queen', 'robot', 'snake', 'tofu',
+         'unicorn', 'vampire', 'wumpus', 'x-ray', 'yak',
+         'zebra']
 
+verbs = ['ate', 'bit', 'caught', 'dropped', 'explained',
+         'fed', 'grabbed', 'hacked', 'inked', 'jumped',
+         'knitted', 'loved', 'made', 'nosed', 'oiled',
+         'puffed', 'quit', 'rushed', 'stung', 'trapped',
+         'uplifted', 'valued', 'wanted']
 
-def silly_string(nouns, verbs, templates):
-    # Choose a random template.
-    template = random.choice(templates)
-
-    # We'll append strings into this list for output.
-    output = []
-
-    # Keep track of where in the template string we are.
-    index = 0
-
-    # Add a while loop here.
-
-    # After the loop has finished, join the output and return it.
-
-
-# To see the results, we need to call the funtion and print what it returns:
-print(silly_string(words.nouns, words.verbs,
-        words.templates))
+templates = [
+        'Waiter! I found a {{noun}} in my {{noun}}!',
+        'The {{noun}} {{verb}} the {{noun}}.',
+        'If you {{verb}} the {{noun}}, '
+        'the {{noun}} will get you.',
+        "Let's go: the {{noun}} is {{verb}}.",
+        'Colorless green {{noun}}s {{verb}} furiously.'
+]
