466d8e2084f5230280b89642483d0506125fae8b
Zachary A. Hampton <95104559+Zollicoff@users.noreply.github.com>
2022-06-13 00:35:18 -0700

    commit

--- Python/Udacity/intro_to_python_1/breakfast_bot/breakfast_bot.py
+++ Python/Udacity/intro_to_python_1/breakfast_bot/breakfast_bot.py
@@ -1,37 +1,37 @@
 import time
 
-print("Hello! I am Sandy, the Breakfast Bot.")
-time.sleep(2)
-print("Today we have two breakfast meals available.")
-time.sleep(2)
-print("The first is waffles with strawberries and whipped cream.")
-time.sleep(2)
-print("The second is sweet potato pancakes with butter and syrup.")
-time.sleep(2)
+def print_pause(message_to_print):
+    print(message_to_print)
+    time.sleep(2)
+
+print_pause("Hello! I am Sandy, the Breakfast Bot.")
+print_pause("Today we have two breakfast meals available.")
+print_pause("The first is waffles with strawberries and whipped cream.")
+print_pause("The second is sweet potato pancakes with butter and syrup.")
 
 while True:
     while True:
         response = input("Please place your order. "
                          "Would you like the waffles or pancakes?\n").lower()
         if "waffles" in response:
-            print("Okay, waffles it is!")
-            time.sleep(2)
+            print_pause("Okay, waffles it is!")
             break
         elif "pancakes" in response:
-            print("Okay, pancakes it is!")
-            time.sleep(2)
+            print_pause("Okay, pancakes it is!")
             break
         else:
-            print("Woah, sorry... I don't understand.")
-            time.sleep(2)
-    print("Your order will be out shortly.")
-    time.sleep(2)
-    order_again = input("Would you like to place another order? "
-                        "Please say 'yes' or 'no'.\n").lower()
-    if "yes" in order_again:
-        print("Very good, I'm happy to take another order!")
-        time.sleep(2)
-    elif "no" in order_again:
-        print("Okay, goodbye!")
-        time.sleep(2)
+            print_pause("Woah, sorry... I don't understand.")
+    print_pause("Your order will be out shortly.")
+    while True:
+        order_again = input("Would you like to place another order? "
+                            "Please say 'yes' or 'no'.\n").lower()
+        if "yes" in order_again:
+            print_pause("Very good, I'm happy to take another order!")
+            break
+        elif "no" in order_again:
+            print_pause("Okay, goodbye!")
+            break
+        else:
+            print_pause("Woah, sorry... I don't understand.")
+    if "no" in order_again:
         break
