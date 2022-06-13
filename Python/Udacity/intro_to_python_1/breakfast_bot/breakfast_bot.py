4f31a595b845e4f102ce0fd4ab840547be15e85a
Zachary A. Hampton <95104559+Zollicoff@users.noreply.github.com>
2022-06-12 22:51:26 -0700

    commit

--- Python/Udacity/intro_to_python_1/breakfast_bot/breakfast_bot.py
+++ Python/Udacity/intro_to_python_1/breakfast_bot/breakfast_bot.py
@@ -6,18 +6,26 @@
 
 import time
 
-print("Hello! I am Bob, the Breakfast Bot. \n"
-      "Today we have two breakfast meals available. \n"
-      "The first is waffles with strawberries and whipped cream. \n"
-      "The second is sweet potato pancakes with butter and syrup.")
+print("Hello! I am Bob, the Breakfast Bot.")
+time.sleep(2)
+print("Today we have two breakfast meals available.")
+time.sleep(2)
+print("The first is waffles with strawberries and whipped cream.")
+time.sleep(2)
+print("The second is sweet potato pancakes with butter and syrup.")
+time.sleep(2)
 while True:
     response = input("Please place your order. "
                      "Would you like the waffles or pancakes?\n").lower()
     if "waffles" in response:
         print("Okay, waffles it is!")
+        time.sleep(2)
         break
     elif "pancakes" in response:
         print("Okay, pancakes it is!")
+        time.sleep(2)
         break
     else:
         print("Woah, sorry... I don't understand.")
+        time.sleep(2)
+print("Your order will be out shortly.")
