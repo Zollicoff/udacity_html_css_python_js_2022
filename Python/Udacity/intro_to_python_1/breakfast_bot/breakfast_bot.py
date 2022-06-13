683561aca4bb9cd9295014b5b1382bba6d9fa37e
Zachary A. Hampton <95104559+Zollicoff@users.noreply.github.com>
2022-06-12 23:10:33 -0700

    commit

--- Python/Udacity/intro_to_python_1/breakfast_bot/breakfast_bot.py
+++ Python/Udacity/intro_to_python_1/breakfast_bot/breakfast_bot.py
@@ -1,12 +1,6 @@
-# Get input and use it to determine what happens next
-# Handle bad input without crashing
-# Be flexible with the input the user can enter
-# Print messages to the console, with a short pause after each one
-# Allow the player to order again if they like
-
 import time
 
-print("Hello! I am Bob, the Breakfast Bot.")
+print("Hello! I am Sandy, the Breakfast Bot.")
 time.sleep(2)
 print("Today we have two breakfast meals available.")
 time.sleep(2)
@@ -14,18 +8,30 @@ print("The first is waffles with strawberries and whipped cream.")
 time.sleep(2)
 print("The second is sweet potato pancakes with butter and syrup.")
 time.sleep(2)
+
 while True:
-    response = input("Please place your order. "
-                     "Would you like the waffles or pancakes?\n").lower()
-    if "waffles" in response:
-        print("Okay, waffles it is!")
+    while True:
+        response = input("Please place your order. "
+                         "Would you like the waffles or pancakes?\n").lower()
+        if "waffles" in response:
+            print("Okay, waffles it is!")
+            time.sleep(2)
+            break
+        elif "pancakes" in response:
+            print("Okay, pancakes it is!")
+            time.sleep(2)
+            break
+        else:
+            print("Woah, sorry... I don't understand.")
+            time.sleep(2)
+    print("Your order will be out shortly.")
+    time.sleep(2)
+    order_again = input("Would you like to place another order? "
+                        "Please say 'yes' or 'no'.\n").lower()
+    if "yes" in order_again:
+        print("Very good, I'm happy to take another order!")
+        time.sleep(2)
+    elif "no" in order_again:
+        print("Okay, goodbye!")
         time.sleep(2)
         break
-    elif "pancakes" in response:
-        print("Okay, pancakes it is!")
-        time.sleep(2)
-        break
-    else:
-        print("Woah, sorry... I don't understand.")
-        time.sleep(2)
-print("Your order will be out shortly.")
