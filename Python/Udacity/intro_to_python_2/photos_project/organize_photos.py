37e1c8e31e1670766c3db1e753a3743a0eb9b2a5
Zachary A. Hampton <95104559+Zollicoff@users.noreply.github.com>
2022-06-26 23:26:06 -0700

    commit

---
+++ Python/Udacity/intro_to_python_2/photos_project/organize_photos.py
@@ -0,0 +1,11 @@
+# 1 Get a list of the file names
+# 2 Extract the place names from the file names
+# 3 Make a directory for each place name
+# 4 Move files into the right directories
+
+# os.listdir(path='.')
+# os.mkdir(path, mode=0o777, *, dir_fd=None)
+# os.rename(src, dst, *, src_dir_fd=None, dst_dir_fd=None)
+
+# os.getcwd()
+# os.chdir(path)
