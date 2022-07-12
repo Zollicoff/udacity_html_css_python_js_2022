a42e8c6de9aa4ac7d0d741bf3acfd7eb6fa52762
Zachary A. Hampton <95104559+Zollicoff@users.noreply.github.com>
2022-07-11 22:34:12 -0700

    commit

---
+++ Python/Udacity/intro_to_python_2/2_photos_project/organize_photos.py
@@ -0,0 +1,22 @@
+import os
+
+
+def extract_place(filename):
+    return filename.split('_')[1]
+
+
+def make_place_directories(places): # Here's the function definition
+    for place in places:
+        os.mkdir(place)
+
+
+os.chdir("Photos")
+originals = os.listdir()
+places = []
+for filename in originals:
+    place = extract_place(filename)
+    if place not in places: # This is the key change
+        places.append(place)
+
+make_place_directories(places) # Don't forget to call the function!
+print(os.listdir()) # This is just a temporary line for testing!
