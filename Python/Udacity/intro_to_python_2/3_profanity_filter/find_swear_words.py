471be8bf16700948a6abbb70a0dd56f1ef87a2e0
Zachary A. Hampton <95104559+Zollicoff@users.noreply.github.com>
2022-08-05 13:06:37 -0700

    commit

--- Python/Udacity/intro_to_python_2/3_profanity_filter/find_swear_words.py
+++
@@ -1,8 +0,0 @@
-# read file contents into a string
-
-# split string into a list of words
-
-# for word in the file-contents list:
-#   is the word IN the rude-words list?
-#         if so, alert
-#         else congratulate
