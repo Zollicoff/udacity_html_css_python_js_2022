54a16e50e5a5d5d45c82d866271f2db479a0b075
Zachary A. Hampton <95104559+Zollicoff@users.noreply.github.com>
2022-08-07 18:59:17 -0700

    commit

---
+++ Python/Udacity/intro_to_python_2/4_webapi/weather_forecast1.py
@@ -0,0 +1,18 @@
+import os
+
+
+# use python reuests module to send requests to a web API
+
+
+
+# use python try and except blocks to handle disruptive events (called exceptions).
+
+
+
+# use a format call JSON to access data from web APIs
+
+
+
+# use python dictionaries to structure data in a key-value pairs
+
+
