2d2a4732cf356833ed5c9c18177236c75f759ee1
Zachary A. Hampton <95104559+Zollicoff@users.noreply.github.com>
2022-08-15 22:03:50 -0700

    commit

---
+++ Udacity/javascript/basic2/jstutorial18_truthy_falsy.js
@@ -0,0 +1,11 @@
+if ("") {
+    console.log("the value is truth");
+} else {
+    console.log("the value is falsy");
+}
+
+if (0.0) {
+    console.log("the value is truthy")
+} else {
+    console.log("the value is falsy")
+}
