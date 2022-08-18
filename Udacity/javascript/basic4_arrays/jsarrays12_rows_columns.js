efcf900753749c96800864225ff4c165ed7d0a50
Zachary A. Hampton <95104559+Zollicoff@users.noreply.github.com>
2022-08-18 15:38:43 -0700

    commit

--- Udacity/javascript/basic4_arrays/jsarrays12_rows_columns.js
+++ Udacity/javascript/basic4_arrays/jsarrays12_rows_columns.js
@@ -3,3 +3,11 @@ for (var row = 0; row < grid.length; r++) {
     console.log(grid[row][column]);
   }
 }
+
+// or 
+
+for (var row = 0; row < donutBox.length; row++) {
+  for (var column = 0; column < donutBox[row].length; column++) {
+    console.log(donutBox[row][column]);
+  }
+}
