ec7ba79424d315dfad108c7fecb48e23b82b14fb
Zachary A. Hampton <95104559+Zollicoff@users.noreply.github.com>
2022-08-17 23:39:55 -0700

    commit

---
+++ Udacity/javascript/basic2/jstutorial37.js
@@ -0,0 +1,7 @@
+function reverseString(reverseMe) {
+  var reversed = "";
+  for (var i = reverseMe.length - 1; i >= 0; i--) {
+    reverse += reverseMe[i];
+  }
+  return reversed;
+}
---
+++ Udacity/javascript/basic3/jsfunctions1.js
@@ -0,0 +1,5 @@
+function laugh() {
+  var sound = "hahahahahahahahahaha!"
+  return sound;
+}
+console.log(laugh());
