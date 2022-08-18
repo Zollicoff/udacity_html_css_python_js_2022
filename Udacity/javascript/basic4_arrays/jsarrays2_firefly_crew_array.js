c1b01244d8ea21b18954a3b15d5e647845f4fbe4
Zachary A. Hampton <95104559+Zollicoff@users.noreply.github.com>
2022-08-18 13:00:17 -0700

    commit

---
+++ Udacity/javascript/basic4_arrays/jsarrays2_firefly_crew_array.js
@@ -0,0 +1,12 @@
+// original
+var captain = "Mal";
+var second = "Zoe";
+var pilot = "Wash";
+var companion = "Inara";
+var mercenary = "Jayne";
+var mechanic = "Kaylee";
+
+// creates array
+var crew = new Array(captain, second, pilot, companion, mercenary, mechanic);
+
+console.log(crew);
