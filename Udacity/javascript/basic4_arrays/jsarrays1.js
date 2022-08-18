54c12f60ad518986b6268dfaab1f20fc2a5eb491
Zachary A. Hampton <95104559+Zollicoff@users.noreply.github.com>
2022-08-18 12:09:54 -0700

    commit

---
+++ Udacity/javascript/basic4_arrays/jsarrays1.js
@@ -0,0 +1,3 @@
+var donuts = ["glazed","chocolate frosted","cinnamon","sprinkled","powdered","cinnamon sugar","glazed cruller","chocolate cruller","cookies","Boston creme","powdered jelly filled","creme de leche","glazed donut holes","blueberry donut holes","cake donut holes","chocolate donus holes"];
+
+console.log(donuts)
