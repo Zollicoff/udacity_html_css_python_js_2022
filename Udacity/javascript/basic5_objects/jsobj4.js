ddee93122351ea30597ed1fd0d76772389300ea4
Zachary A. Hampton <95104559+Zollicoff@users.noreply.github.com>
2022-08-18 18:39:56 -0700

    commit

---
+++ Udacity/javascript/basic5_objects/jsobj4.js
@@ -0,0 +1,38 @@
+/*
+ * Programming Quiz: Facebook Friends (7-5)
+ */
+
+/*
+ * QUIZ REQUIREMENTS
+ * - Your code should have an object `facebookProfile`
+ * - The `facebookProfile` object should have the `name` (string), `friends` (number), and `messages` (array of strings) property
+ * - Your `facebookProfile` object should have the `postMessage()`, `deleteMessage()`, `addFriend()` and `removeFriend()` method
+ * - Carefully implement the desired functionality of each method, as decribed above
+ */
+
+
+// TIP - 
+// In an array, 
+// - addition at the end is done using push() method
+// - addition at a specific index is done using splice() method
+// - deletion from the beginning is done using pop() method
+// - deletion from a specific index is done using splice() method
+
+// your code goes here
+var facebookProfile = {
+    name: "Zach",
+    friends: 3,
+    messages: ["StarWars", "StarTrek", "VideoGames"],
+    postMessage: function(message){
+        facebookProfile.messages.push(message);
+    },
+    deleteMessage: function(index){
+        facebookProfile.messages.splice(index, 1);
+    },
+    addFriend: function(){
+        facebookProfile.friends = facebookProfile.friends + 1;
+    },
+    removeFriend: function(){
+        facebookProfile.friends = facebookProfile.friends - 1;
+    }
+};
