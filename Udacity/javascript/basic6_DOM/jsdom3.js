34ce1e9156f1649df7cf53bfbbcd1fb4a789a157
Zachary A. Hampton <95104559+Zollicoff@users.noreply.github.com>
2022-08-19 17:27:05 -0700

    commit

---
+++ Udacity/javascript/basic6_DOM/jsdom3.js
@@ -0,0 +1,9 @@
+// ran in the terminal with Udacity downloaded website
+
+const container = document.createElement('span');
+
+container.textContent = ', right now!';
+
+const mainHeading = document.querySelector('h1');
+
+mainHeading.appendChild(container);
